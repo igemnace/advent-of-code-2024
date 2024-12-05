4f1439075ee5740d8c47af3ee6d42d09d37422f0
Ian Emnace <igemnace@gmail.com>
2024-12-05 13:57:23 +0800

    Add Day 3 Part 2 with Racket
    I'm hurting for destructuring syntax... but oh well, that can be covered
    by packages.
    
    I also had an eq? vs equal? confusion moment. I actually had it in my
    head that "oh, I can just use eq?, I don't have to remember eq/equal/=
    like in Common Lisp", only for me to realize that's also actually the
    case for Racket haha. But at least I have good intuition now for the
    special cases. It goes eq? -> eqv? -> equal?.
    
    Let's see if that sticks with me! If so, maybe Racket's docs present it
    more clearly. Otherwise, maybe it's just the language...

--- day03/day03.rkt
+++ day03/day03.rkt
@@ -35,40 +35,80 @@ and don't() directives, so separate parser and eval steps will help.
 
 I'm skipping a separate lexer step cuz token -> AST is gonna be pretty simple
 (pretty much just "mul(X,Y)" -> '(mul X Y))
+
+Part 2:
+
+There is now a do() instruction that enables mul(), and a don't() instruction
+that disables it. So, for example,
+
+don't()mul(1,1)do()
+
+would actually print 0. Only the most recent do()/don't() instruction applies,
+so I don't need to think about nesting or closing.
+
+This can easily be done by extending the parse+eval interpreter to simply
+understand those two new instructions. Amendments to the algorithm above:
+
+2. We now also parse do() and don't() into their own instructions.
+3.
+  - Apart from a sum count, we keep a "do" flag. We only eval a mul as a
+    product if "do" is true; otherwise it just evals to 0.
+  - We eval a do by toggling the "do" flag, and inversely for don't
 |#
 
 ;; PARSE
 (define (parse-instruction match)
-  (cond
-    [(string-prefix? (first match) "mul")
+  (let ([op (car match)])
+    (cond
       ; '(mul X Y)
-      (cons 'mul (map string->number (rest match)))]
-    [else '()]))
+      [(string-prefix? (car match) "mul")
+       (cons 'mul (map string->number (rest match)))]
+      ; '(do)
+      [(equal? (car match) "do()")
+       '(do)]
+      ; '(dont)
+      [(equal? (car match) "don't()")
+       '(dont)]
+      [else '()])))
 
 (define (parse-instructions str)
   ; NOTE: Thank you, regexp-quote! Pretty easy to inspect in the REPL how
   ; certain characters have to be quoted
   ; regexp-match* is our lexer :P
-  (let ([matches (regexp-match* #rx"mul\\(([0-9]+),([0-9]+)\\)" str #:match-select values)])
+  (let ([matches (regexp-match* #rx"mul\\(([0-9]+),([0-9]+)\\)|do\\(\\)|don't\\(\\)" str #:match-select values)])
     (map parse-instruction matches)))
 
 ;; EVAL
-(define (eval-instruction instruction)
-  (cond
-    ; mul: simply multiply
-    [(eq? (car instruction) 'mul) (foldl * 1 (cdr instruction))]))
+; return both count and the do flag
+(define (eval-instruction do? instruction)
+  (let ([op (car instruction)]
+        [args (cdr instruction)])
+    (cond
+      ; mul: multiply only if do flag is true
+      [(eq? op 'mul)
+       (if do?
+           (values (foldl * 1 args) do?)
+           (values 0 do?))]
+      ; do: set do flag to true
+      [(eq? op 'do)
+       (values 0 #t)]
+      ; dont: set do flag to false
+      [(eq? op 'dont)
+       (values 0 #f)])))
 
 ;; use an accumulating recursive function
-;; acc is just the count
+;; acc is a pair of count and the do flag
 (define (eval-instructions-acc acc instructions)
-  (cond
-    [(empty? instructions) acc]
-    [#t (let ([result (eval-instruction (car instructions))])
-          ; simply sum the results as we go on
-          (eval-instructions-acc (+ acc result) (cdr instructions)))]))
+  (let ([count (car acc)]
+        [do? (cdr acc)])
+    (cond
+      [(empty? instructions) count]
+      [#t (let-values ([(c d?) (eval-instruction do? (car instructions))])
+            ; simply sum the results as we go on, and thread the do flag
+            (eval-instructions-acc (cons (+ count c) d?) (cdr instructions)))])))
 
 (define (eval-instructions instructions)
-  (eval-instructions-acc 0 instructions))
+  (eval-instructions-acc '(0 #t) instructions))
 
 ;; MAIN
 ;; functional core, imperative shell...
